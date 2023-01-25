61a092349b7a2066b009d3eb6b412431daa3356e
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2023-01-25 13:12:31 +0100

    add style to map

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 
 import "leaflet/dist/leaflet.css"; // <- Leaflet styles
+import "mapbox-gl/dist/mapbox-gl.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
