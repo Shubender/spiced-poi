6b65207528a6ad17d6289e78daf73ebaaf12e938
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2023-01-27 17:06:55 +0100

    import bootstrap.min.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "leaflet/dist/leaflet.css"; // <- Leaflet styles
 import "mapbox-gl/dist/mapbox-gl.css";
