8c522976227e5a3266e1cba014bbd0b1e3bcdb8b
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2023-01-29 10:47:47 +0100

    update Uploader

--- src/Uploader.jsx
+++ src/Uploader.jsx
@@ -10,19 +10,54 @@ export default function Uploader(props) {
     const handleClose = () => setShow(false);
     // const handleShow = () => setShow(true);
 
+    function checkInput(event) {
+        const value = document.getElementById("validationTextarea").value;
+        console.log("value: ", value);
+
+        if (value.length < 10 || value.length > 140) {
+            // event.target.classList.remove("is-valid");
+            event.target.classList.add("is-invalid");
+        } else {
+            event.target.classList.remove("is-invalid");
+            event.target.classList.add("is-valid");
+        }
+    }
+
     return (
         <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Add new place</Modal.Title>
+            </Modal.Header>
             <Form onSubmit={props.handleSubmitUpload}>
-                <Form.Group controlId="formFile" className="m-3 w-75">
-                    <Form.Label>Upload your pic here:</Form.Label>
+                <Form.Group className="m-3 w-75">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Type your thought about this place"
+                        required
+                        id="validationTextarea"
+                        className="form-control"
+                        onKeyUp={checkInput}
+                    />
+                    <Form.Text id="textLong" muted>
+                        From 10 to 140 characters
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="m-3 w-75">
+                    <Form.Label>Upload your pic here</Form.Label>
                     <Form.Control
                         type="file"
                         name="file"
                         accept="image/*"
                         onChange={props.handleFileChange}
+                        required
                     />
+                    <Form.Text id="lngLatText" muted>
+                        Longitude: {props.userClick.lng.toFixed(5)} | Latitude:{" "}
+                        {props.userClick.lat.toFixed(5)}
+                    </Form.Text>
                 </Form.Group>
-                <Button variant="primary" type="submit" className="m-3">
+                <Button variant="outline-primary" type="submit" className="m-3">
                     Upload
                 </Button>
             </Form>
