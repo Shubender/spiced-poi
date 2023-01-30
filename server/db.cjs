7b6fef37920b8d0bfd5a50b5da7aa1b1dd3270ae
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2023-01-30 13:24:58 +0100

    add createPlace func

--- server/db.cjs
+++ server/db.cjs
@@ -6,3 +6,17 @@ const db = spicedPg(DATABASE_URL);
 module.exports.getPlaces = () => {
     return db.query(`SELECT * FROM places`);
 };
+
+module.exports.createPlace = (
+    imgUrl,
+    description,
+    filename,
+    lng,
+    lat,
+    color
+) => {
+    return db.query(
+        `INSERT INTO places (url, description, name, longitude, latitude, color) VALUES($1, $2, $3, $4, $5, $6) RETURNING *`,
+        [imgUrl, description, filename, lng, lat, color]
+    );
+};
